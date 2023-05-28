c4ae6e389eeeca81ec4e00dc3dc523d603a897e2
Keenan-Adityo <keenanadityo167@gmail.com>
2023-05-28 17:52:52 +0700

    make unfurnished homepage

---
+++ resources/views/home.blade.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body>
+    <h1 class="text-center text-4xl font-semibold">
+          Keenan's Portfolio
+    </h1>
+    <p class="font-semibold text-2xl">Mobile Apps</p>
+    <!-- diisi list mobile apps redirect ke detail apps -->
+    <p class="font-semibold text-2xl">Web Apps</p>
+    <!-- diisi list web apps redirect ke detail apps -->
+</body>
+</html>
