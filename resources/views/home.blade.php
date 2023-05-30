d65cb2382d81baf5f5e4fa861ba6bcd2c4de2fae
Keenan-Adityo <keenanadityo167@gmail.com>
2023-05-30 22:18:11 +0700

    add navbar and title

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,12 +6,26 @@
   <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body>
-    <h1 class="text-center text-4xl font-semibold">
-          Keenan's Portfolio
-    </h1>
-    <p class="font-semibold text-2xl">Mobile Apps</p>
+<nav class="flex justify-between space-x-4">
+  <div>
+    <a href="/dashboard" class="font-bold px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900">About Me</a>
+    <a href="/team" class="font-bold px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900">Portfolio</a>
+    <!-- <a href="/projects" class="font-bold px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900">Projects</a> -->
+  </div>
+  <a href="/reports" class="font-bold px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900">Contact Us</a>
+</nav>
+<div class="pl-20">
+  <h1 class="text-5xl font-normal">
+        My name
+  </h1>
+  <h1 class="text-5xl font-normal">
+        is <b class="font-semibold">Keenan</b>
+  </h1>
+</div>
+    <p class="font-semibold text-3xl">Latest Mobile Apps </p>
     <!-- diisi list mobile apps redirect ke detail apps -->
-    <p class="font-semibold text-2xl">Web Apps</p>
+    <button>dasinfasbfakj</button>
+    <p class="font-semibold text-3xl">Web Apps</p>
     <!-- diisi list web apps redirect ke detail apps -->
 </body>
 </html>
